754ad0025c750c65976f4536c72196f782edaf1f
Moritz Schmidt <fusselwurm@gmail.com>
2011-06-08 22:25:23 +0200

    create structured data from single vcard. meh, still need to do escaping.

--- src/vcard.js
+++ src/vcard.js
@@ -1,8 +1,7 @@
 /**
- * what do I want?
- *
- * create a file from data
- *
+ * limitations:
+ *   no escaping
+ *   no folding
  */
 
 
@@ -68,7 +67,7 @@ var outputFuncs = {
  * @endcode
  *
  */
-exports.createFromData = function (data) {
+exports.createFromData = function (data, version) {
 	return {
 		getData: function () {
 			return data;
@@ -76,6 +75,15 @@ exports.createFromData = function (data) {
 		getProperty: function (name) {
 			return data[name];
 		},
+		getVersion: function () {
+			return version;
+		},
+		/**
+		 * get vCard string.
+		 * @note does not do folding, yet
+		 *
+		 * @return string
+		 */
 		toVCard: function () {
 			var
 				f = outputFuncs;
@@ -90,8 +98,70 @@ exports.createFromData = function (data) {
 
 /**
  *
- * @param data (string) vcard contents
+ * @param vCard string contents of a single vcard
  */
-exports.createFromVCard = function (data) {
+exports.createFromVCard = function (vCard) {
+	var
+		// unfold, then split into logical lines
+		lines = vCard.trim().replace(/\r\n\s+/, '').split('\r\n'),
+		//
+		// 0.. before VCARD:BEGIN,
+		// 4.. within VCARD, before VERSION,
+		// 5.. within VCARD,
+		// 9.. AFTER VCARD:END
+		scope = 0,
+		data,
+		version = '0';
 
+	if (lines.shift().trim() !== 'BEGIN:VCARD') {
+		throw new Error('vcard must have BEGIN:VCARD statement first');
+	}
+
+	if (lines.pop().trim() !== 'END:VCARD') {
+		throw new Error('vcard must have END:VCARD statement last');
+	}
+
+	version = lines.shift().trim().match(/^version:([0-9\.]+)$/i);
+	if (!version) {
+		throw new Error('vcard must have VERSION statement directly after BEGIN');
+	} else {
+		version = version[1];
+	}
+
+	// great! now all other lines contain values:
+
+	data = lines.map(function (line) {
+		var linebits,
+			tmp,
+			tmp2,
+			result = {};
+
+		line = line.trim();
+		if (!line) {
+			// dont care for empty lines. at all.
+			throw '*confused* waiiiit.. didnt I filter out those empty with the unfolding? I think I did.';
+		}
+
+		linebits = line.split(/:/, 2);
+
+		// group, name, param
+		tmp = linebits[0].split(';');
+
+		tmp2 = tmp.shift().split('.', 2);
+		result.name = tmp2.pop();
+		if (tmp2.length) {
+			result.group = tmp2.pop();
+		}
+
+		tmp.forEach(function (p) {
+			var bits = p.split('=', 2);
+			result.param = result.param || {};
+			result.param[bits[0]] = bits[1].split(',');
+		});
+
+		// value
+		result.value = linebits[1].split(',');
+		return result;
+	});
+	return data;
 };
